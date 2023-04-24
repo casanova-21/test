3c315a110e5d88aa52eddcd48a82712dd20c06cf
John Casanova <85595214+casanova-21@users.noreply.github.com>
2023-04-24 11:30:52 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -10,8 +10,8 @@
     <div id="temp"></div>
   </p>
   <script type="text/javascript">
-    let untrusted_ajax_res = document.write(location.replace("https://www.w3schools.com"));
-    document.getElementById("temp").innerHTML=untrusted_ajax_res;
+    let untrusted_ajax_res = location.replace("https://www.w3schools.com");
+    document.getElementById("temp").innerHTML = untrusted_ajax_res;
   </script>
 </body>
 </html>
